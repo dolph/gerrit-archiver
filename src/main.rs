ee047911f639babdadae41572237e1e3849ddc96
Dolph Mathews <dolph.mathews@gmail.com>
2017-01-10 07:44:56 -0600

    Add options for interacting with gerrit

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ extern crate argparse;
 
 struct Options {
     verbose: bool,
+    gerrit_review_number: u64,
+    gerrit_host: String,
+    gerrit_port_number: u16,
+    gerrit_username: String,
+    identity_file: String,
 }
 
 fn main() {
@@ -14,6 +19,11 @@ fn main() {
     // Initialize options.
     let mut options = Options {
         verbose: false,
+        gerrit_review_number: 1,
+        gerrit_host: "".to_string(),
+        gerrit_port_number: 29418,
+        gerrit_username: "".to_string(),
+        identity_file: "~/.ssh/id_rsa".to_string(),
     };
 
     // Parse command line arguments.
@@ -30,6 +40,21 @@ fn main() {
         parser.refer(&mut options.verbose)
             .add_option(&["-v", "--verbose"], argparse::StoreTrue,
             "Enable verbose output.");
+        parser.refer(&mut options.gerrit_host)
+            .add_argument("gerrit-host", argparse::Store,
+            "Gerrit username.");
+        parser.refer(&mut options.gerrit_port_number)
+            .add_argument("gerrit-port", argparse::Store,
+            "Gerrit port number.");
+        parser.refer(&mut options.gerrit_username)
+            .add_option(&["-u", "--username"], argparse::Store,
+            "Gerrit username.");
+        parser.refer(&mut options.identity_file)
+            .add_option(&["-i", "--identity"], argparse::Store,
+            "Path to SSH identity file.");
+        parser.refer(&mut options.gerrit_review_number)
+            .add_argument("review-number", argparse::Store,
+            "Review to archive.");
         parser.parse_args_or_exit();
     }
 
