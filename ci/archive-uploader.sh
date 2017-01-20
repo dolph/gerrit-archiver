78cf83d0b8b81e47ff71838f26342ecde144f9de
Dolph Mathews <dolph.mathews@gmail.com>
2017-01-20 11:26:29 -0600

    Add uploader

---
+++ ci/archive-uploader.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+ENDPOINT=$1
+RACK_CONTAINER=$2
+TOKEN=$3
+f=$4
+
+for i in `seq 1 3`;
+do
+    curl \
+        --silent \
+        --request PUT \
+        $ENDPOINT/$RACK_CONTAINER/`basename $f` \
+        --header "X-Auth-Token: $TOKEN" \
+        --header "Content-Type: application/json" \
+        --upload-file $f \
+        && break
+done
